9d427901224a77e46582739dbc0dd3fa52a417e4
user26 <pedro.pinheiro@gsuite.iff.edu.br>
2026-04-01 17:27:48 -0300

    Inicializando Projeto Spring

---
+++ econsig/src/main/java/com/pp/econsig/EconsigApplication.java
@@ -0,0 +1,13 @@
+package com.pp.econsig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EconsigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EconsigApplication.class, args);
+	}
+
+}
